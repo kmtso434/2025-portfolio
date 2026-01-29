f4391e1509bf64e487cf3dd388747d2e03a940ff
kmtso434 <122084476+kmtso434@users.noreply.github.com>
2026-01-30 00:11:27 +0900

    feat: Add skill section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { type Component } from "solid-js";
-import { FirstSection } from "./pages/FirstSection";
+import { For, type Component } from "solid-js";
+import { FirstSection } from "./pages/MainPage/FirstSection";
 
 const App: Component = () => {
   return (
@@ -31,6 +31,116 @@ const App: Component = () => {
       >
         {/* First Section */}
         <FirstSection id="FirstSection" />
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            padding: "0rem 3% 3% 3%",
+            "font-family": "NimbusSanL-Bol",
+            "font-weight": "bold",
+            "font-size": "clamp(1rem, 3vw, 3rem)",
+            color: "#ffffff",
+            opacity: "0.5",
+          }}
+        >
+          <span>Skills & Tools</span>
+
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              height: `calc(100% - 3rem)`,
+            }}
+          >
+            <div style={{ width: "50%", height: "100%" }}>
+              <For
+                each={[
+                  { key: "UI & UX Design", percent: 70 },
+                  { key: "Web Development", percent: 80 },
+                  { key: "Embedding Binaries", percent: 80 },
+                  { key: "Plot", percent: 70 },
+                ]}
+              >
+                {(skill) => {
+                  return (
+                    <div
+                      style={{
+                        width: "100%",
+                        display: "grid",
+                        gridTemplateColumns: "1fr 1fr",
+                        gridTemplateRows: "1fr 1fr",
+                        "row-gap": "0.5rem",
+                        "margin-top": "0.5rem",
+                        "font-weight": "light",
+                        "font-size": "clamp(1rem, 2vw, 2rem)",
+                      }}
+                    >
+                      <span style={{ "grid-row": 1, "grid-column": 1 }}>
+                        {skill.key}
+                      </span>
+                      <span style={{ "grid-row": 1, "grid-column": 2 }}>
+                        {skill.percent}%
+                      </span>
+                      <div
+                        style={{
+                          "grid-row": 2,
+                          "grid-column": "1 / span 2",
+                          width: "100%",
+                          height: "0.5rem",
+                          background: "#ffffff",
+                        }}
+                      />
+                      <div
+                        style={{
+                          "grid-row": 2,
+                          "grid-column": "1 / span 2",
+                          width: `${skill.percent}%`,
+                          height: "0.5rem",
+                          background: "hsla(39,88%,47%,1)",
+                        }}
+                      />
+                    </div>
+                  );
+                }}
+              </For>
+            </div>
+
+            <div style={{ width: "50%", "margin-left": "2rem" }}>
+              <span style={{ "margin-bottom": "2rem" }}>
+                {"Tools | Work with"}
+              </span>
+              <div className="flex flex-wrap gap-3" style={{ height: "25%" }}>
+                <For
+                  each={[
+                    "uplot",
+                    "tauri",
+                    "solid js",
+                    "github",
+                    "figma",
+                    "typescript",
+                    "rust",
+                  ]}
+                >
+                  {(tool) => {
+                    return (
+                      <span
+                        style={{
+                          padding: "0.5rem",
+                          "border-width": "1px",
+                          "border-color": "#ffffff",
+                          "line-height": "1",
+                          //height: "1rem",
+                        }}
+                      >
+                        {tool}
+                      </span>
+                    );
+                  }}
+                </For>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       {/*Top bar */}
       <div
